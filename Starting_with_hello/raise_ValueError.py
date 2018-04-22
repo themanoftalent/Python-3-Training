6fa4520faee1728947a742c854623801f7535509
Akif Ciftci <akifiction@gmail.com>
2018-04-22 20:09:23 +0300

    raise_ValueError

---
+++ Starting_with_hello/raise_ValueError.py
@@ -0,0 +1,15 @@
+def fact(number):
+    number = int(number)
+    if number < 0:
+        raise ValueError("Negative Value")
+    pickNum = 1
+    i = 1
+    while i <= number:
+        pickNum *= i
+        i += 1
+    return pickNum
+
+
+print("It is : ", fact(int(input("Enter a number to calculate factorial value : \n"))))
+
+print(7*720)
