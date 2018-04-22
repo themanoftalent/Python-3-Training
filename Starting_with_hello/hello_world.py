17c0e77d7224719f2c9c1f2f1d89cbada6debba0
Akif Ciftci <akifiction@gmail.com>
2018-04-22 20:19:14 +0300

    hello_akif

--- Starting_with_hello/hello_world.py
+++ Starting_with_hello/hello_world.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
-name="Akif"
-print("Hello, World!",name)
+name = "Akif"
+age = 32
+if age > 40:
+    print("you are younger than that")
+else:
+    print("Hello {}, you are {} years old that is too old to love for me ".format(name, age))  # formatting the string
+
+print("my name" + " " + "is no")
+
+my_name = "No,"
+my_surname = "oh god what am I wriitng "
+print(my_name + my_surname)
+
+astr = "13"
+print(astr)
+# print(astr+13) #raising error, TypeError: must be str, not int
+
+# print(int(astr+12)) #print(int(astr+12))
